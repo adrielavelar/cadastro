5893de64e6def67da943706a7bc89f6ee303d78e
Adriel Amaral <adriel.amaral@rarolabs.com.br>
2024-02-26 08:48:59 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../common/components/header/header.component';
+import { FooterComponent } from '../common/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
